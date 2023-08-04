3e527d5701abec8f624bf88c3ec07db768ab7c27
GrumpyDucc <j.waldinger@online.de>
2023-08-04 13:42:03 +0200

    -

--- 2022/Day 8/Part 1.py
+++ 2022/Day 8/Part 1.py
@@ -1,21 +1,18 @@
-rows = open("2022\\Day 8\\data.txt", "r").read().split("\n")
+rows = open("2022\\Day 8\\testdata.txt", "r").read().split("\n")
 
 def checkTree(currentTreeRow, currentTree):
-    prevRowTrees = [*rows[currentTreeRow-1]][:-1]
-    currRowTrees = [*rows[currentTreeRow]][:-1]
-    nextRowTrees = [*rows[currentTreeRow+1]][:-1]
+    prevRowTrees = [*rows[currentTreeRow-1]]
+    currRowTrees = [*rows[currentTreeRow]]
+    nextRowTrees = [*rows[currentTreeRow+1]]
     tree = currRowTrees[currentTree]
-    if prevRowTrees[currentTree] <= tree:
-        if currRowTrees[currentTree-1] <= tree:
-            if currRowTrees[currentTree+1] <= tree:
-                if nextRowTrees[currentTree] <= tree: return 1 # Tree is visible
-                else: return 0
-            else: return 0
-        else: return 0
-    else: return 0
+    if prevRowTrees[currentTree] >= tree and currRowTrees[currentTree-1] >= tree and currRowTrees[currentTree+1] >= tree and nextRowTrees[currentTree] >= tree:
+        return 0    # Tree is not visible
+    else:
+        return 1    # Tree is visible
 
+# Die GANZE reihe testen nicht nur die angrenzenden
 
-visibleTrees = len(rows)*len(rows[0])
+visibleTrees = 0
 
 currentRow = 1
 currentTree = 1
@@ -23,9 +20,10 @@ currentTree = 1
 while currentRow < len(rows)-1:
     row = [*rows[currentRow]][:-1]
     currentTree = 0
-    while currentTree < len(row)-2:
-        visibleTrees += checkTree(currentRow, currentTree)
+    while currentTree < len(row)-1:
         currentTree += 1
+        visibleTrees += checkTree(currentRow, currentTree)
     currentRow += 1
 
-print(visibleTrees)
+outerRing = (len(rows)-2)*2+len(rows[0])*2
+print(visibleTrees, outerRing)
