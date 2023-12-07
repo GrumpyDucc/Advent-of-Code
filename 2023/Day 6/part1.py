845596902ad0d21de4e1b72146f33864fc28dafc
GrumpyDucc <j.waldinger@online.de>
2023-12-07 20:20:59 +0100

    day 6

---
+++ 2023/Day 6/part1.py
@@ -0,0 +1,27 @@
+input = open("2023/Day 6/input.txt", "r").read().split('\n')
+endValue = 1
+
+totalWins = []
+times = input[0].split(':')[1].split(' ')
+times = [j for i,j in enumerate(times) if j!='']
+distances = input[1].split(':')[1].split(' ')
+distances = [j for i,j in enumerate(distances) if j!='']
+
+for i in range(len(times)):
+    duration = int(times[i])
+    recordDistance = int(distances[i])
+    totalWinningHoldTimes = []
+    for j in range(duration):        
+
+        holdTime = j
+        remainTime = duration - holdTime
+        distance = remainTime * holdTime
+
+        if distance > recordDistance:
+            totalWinningHoldTimes.append(holdTime)
+    totalWins.append(len(totalWinningHoldTimes))          
+
+for win in totalWins:
+    endValue *= win
+
+print(endValue)
