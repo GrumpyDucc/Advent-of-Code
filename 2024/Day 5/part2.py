6cb65cbf43fe64ceded48b95d9ce2eb3f759a533
GrumpyDucc <j.waldinger@online.de>
2024-12-05 08:37:48 +0100

    day 5 template

---
+++ 2024/Day 5/part2.py
@@ -0,0 +1,6 @@
+input = open("2024/Day 5/input.txt", "r").readlines()
+endValue = 0
+
+
+
+print(endValue)
---
+++ Day x/part2.py
@@ -0,0 +1,6 @@
+input = open("2024/Day X/input.txt", "r").readlines()
+endValue = 0
+
+
+
+print(endValue)
