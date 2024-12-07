410e438239907d25d3db185a443db635bbada47b
GrumpyDucc <j.waldinger@online.de>
2024-12-07 18:21:39 +0100

    Co-authored-by: mightytry <mightytry@users.noreply.github.com>

--- 2024/Day 6/part1.py
+++ 2024/Day 6/part1.py
@@ -1,19 +1,21 @@
+import time
 from aocd import get_data
 
-USE_TEST_INPUT = True
-if USE_TEST_INPUT: input = open("2024/Day 6/tinput.txt", 'r').read()
+USE_TEST_INPUT = False
+if USE_TEST_INPUT: input = open("2024/Day 6/tinput.txt", 'r').read().strip()
 else: input = get_data(day=6, year=2024)
 endValue = 0
+
+start = time.time()
 
 map = [list(x)for x in input.splitlines()]
 
 def findGuard() -> list[int]:
     for r in range(len(map)):
-        if map[r].count('^') > 0: return [r, map[r].index('^')]
+        if '^' in map[r]: return [r, map[r].index('^')]
     return []
 
 def isBlocked(coords: list[int]):
-    if coords[0] not in range(len(map)) or coords[1] not in range(len(map)): return False
     if map[coords[0]][coords[1]] == "#": return True
     return False
 
@@ -23,30 +25,23 @@ guardDirection = 0
 visitedCoords = [guardCoords]
 
 while not guardLeft:
-    match guardDirection:
+    match guardDirection % 4:
         case 0: # Up = 0
             newCoords = [guardCoords[0]-1, guardCoords[1]]
-            if isBlocked(newCoords):
-                guardDirection = 1
-            else: guardCoords = newCoords
         case 1: # Right = 1
             newCoords = [guardCoords[0], guardCoords[1]+1]
-            if isBlocked(newCoords):
-                guardDirection = 2
-            else: guardCoords = newCoords
         case 2: # Down = 2
             newCoords = [guardCoords[0]+1, guardCoords[1]]
-            if isBlocked(newCoords):
-                guardDirection = 3
-            else: guardCoords = newCoords
         case 3: # Left = 3
             newCoords = [guardCoords[0], guardCoords[1]-1]
-            if isBlocked(newCoords): 
-                guardDirection = 0
-            else: guardCoords = newCoords
+    
+    if isBlocked(newCoords): guardDirection += 1
+    else: guardCoords = newCoords
     if guardCoords[0] not in range(len(map)-1) or guardCoords[1] not in range(len(map)-1): guardLeft = True
-    if visitedCoords.count(guardCoords) < 1: 
-        visitedCoords.append(guardCoords)
+    if guardCoords not in visitedCoords: visitedCoords.append(guardCoords)
+
+end = time.time()
+print(end-start)
 
 endValue = len(visitedCoords)
 
--- 2024/Day 6/part2.py
+++ 2024/Day 6/part2.py
@@ -1,10 +1,74 @@
+import time
 from aocd import get_data
 
-USE_TEST_INPUT = True
-if USE_TEST_INPUT: input = open("2024/Day 6/tinput.txt", 'r').read()
+USE_TEST_INPUT = False
+if USE_TEST_INPUT: input = open("2024/Day 6/tinput.txt", 'r').read().strip()
 else: input = get_data(day=6, year=2024)
 endValue = 0
 
+start = time.time()
 
+map = [list(x)for x in input.splitlines()]
+
+def getGuardPosition() -> list[int]:
+    for r in range(len(map)):
+        if '^' in map[r]: return [r, map[r].index('^')]
+    return []
+
+
+startingPosition = getGuardPosition()
+position = startingPosition
+guardReachedBoundary = False
+facing = 0
+visitedCoords = [position]
+
+while not guardReachedBoundary:
+    match facing % 4:
+        case 0: # up = 0
+            nextPosition = [position[0]-1, position[1]]
+        case 1: # right = 1
+            nextPosition = [position[0], position[1]+1]
+        case 2: # down = 2
+            nextPosition = [position[0]+1, position[1]]
+        case 3: # left = 3
+            nextPosition = [position[0], position[1]-1]
+    
+    if map[nextPosition[0]][nextPosition[1]] == "#": facing += 1
+    else: position = nextPosition
+    if position[0] not in range(len(map)-1) or position[1] not in range(len(map)-1): guardReachedBoundary = True
+    if position not in visitedCoords: visitedCoords.append(position)
+
+for positionToBlock in visitedCoords[1:]:
+    position = startingPosition
+    visitedCoordsWithFacing = [[position, 0]]
+    facing = 0
+    while True:
+        match facing:
+            case 0: # up = 0
+                nextPosition = [position[0]-1, position[1]]
+            case 1: # right = 1
+                nextPosition = [position[0], position[1]+1]
+            case 2: # down = 2
+                nextPosition = [position[0]+1, position[1]]
+            case 3: # left = 3
+                nextPosition = [position[0], position[1]-1]
+        
+        if map[nextPosition[0]][nextPosition[1]] == "#" or positionToBlock == nextPosition: 
+            if facing == 3: facing = 0
+            else: facing += 1
+            if [position, facing] not in visitedCoordsWithFacing: 
+                visitedCoordsWithFacing.append([position, facing])
+            else: 
+                endValue += 1
+                print(endValue)
+                break
+        else: 
+            position = nextPosition
+        
+        if position[0] not in range(len(map)-1) or position[1] not in range(len(map)-1):            
+            break
+
+end = time.time()
+print(end-start)
 
 print(endValue)
