56319f1b255494f6bda1774852022f9b4ff97da6
GrumpyDucc <j.waldinger@online.de>
2024-12-09 20:48:48 +0100

    day x

--- Day x/part1.py
+++ Day x/part1.py
@@ -1,10 +1,5 @@
-from aocd import get_data
-
-USE_TEST_INPUT = True
-if USE_TEST_INPUT: input = open("2024/Day X/tinput.txt", 'r').read()
-else: input = get_data(day=X, year=2024)
+from utils.input_handling import loadDay
+input = loadDay(X, 2024, False)
 endValue = 0
-
-
 
 print(endValue)
