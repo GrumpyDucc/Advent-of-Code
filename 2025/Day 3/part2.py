a9ceef918e629c305a40e70a250c750489075053
GrumpyDucc <grumpyduckmail@gmail.com>
2025-12-04 17:40:27 +0100

    2025 d3p2 try 2

--- 2025/Day 3/part2.py
+++ 2025/Day 3/part2.py
@@ -1,22 +1,24 @@
+from itertools import combinations
 
-banks = open("2025/Day 3/input.txt", 'r').read().split('\n')
+banks = open("2025/Day 3/tinput.txt", 'r').read().split('\n')
 
 joltageSum = 0
+cnt = 0
 
 for bank in banks:
     length = len(bank) - 2
     maxJoltage = 0
-    allPossibleBanks: list[int] = []
 
-    for i in range(length):
-        for j in range(length):
-            for k in range(length):
-                testBank = list(bank)
-                testBank.pop(i)
-                testBank.pop(j)
-                testBank.pop(k)
-                testJoltage = int(''.join(testBank))
-                if(testJoltage > maxJoltage): maxJoltage = testJoltage
+    for batteriesToRemove in combinations(range(len(bank)), 3):
+        testBank = list(bank)
+        for index in sorted(batteriesToRemove, reverse=True):
+            testBank.pop(index)
+        
+        testJoltage = int(''.join(testBank))
+        if(testJoltage > maxJoltage): maxJoltage = testJoltage
+
+    cnt += 1
+    print(cnt)
 
     joltageSum += maxJoltage
 
