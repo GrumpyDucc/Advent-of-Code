bbd430e7148a1afaab5b9ef568f6563f41e9cc3d
GrumpyDucc <grumpyduckmail@gmail.com>
2025-12-04 17:15:09 +0100

    2025 d4

---
+++ 2025/Day 4/part2.py
@@ -0,0 +1,41 @@
+diagram = list(map(lambda x: list(x), open("2025/Day 4/input.txt", 'r').read().split('\n')))
+
+directions = [
+    (-1, -1), (-1, 0), (-1, 1), # top-left, top, top-right
+    (0, -1),           (0, 1),  # left, right
+    (1, -1),  (1, 0),  (1, 1)   # bottom-left, bottom, bottom-right
+]
+
+rows = len(diagram)
+cols = len(diagram[0])
+movableRolls = 0
+
+def getSurroundings(row: int, col: int) -> list[str]:
+    neighbors: list[str] = []
+    
+    for dr, dc in directions:
+        new_row = row + dr
+        new_col = col + dc
+        
+        # Check if the neighbor is within bounds
+        if 0 <= new_row < rows and 0 <= new_col < cols:
+            neighbors.append(diagram[new_row][new_col])
+    
+    return neighbors
+
+rollsWereMoved = True
+
+while(rollsWereMoved):
+    rollsWereMoved = False
+
+    for row in range(rows):
+        for col in range(cols):
+            if (diagram[row][col] != '@'): continue
+
+            adjacentRolls = getSurroundings(row, col).count('@')
+            if adjacentRolls < 4: 
+                movableRolls += 1
+                rollsWereMoved = True
+                diagram[row][col] = '.'
+
+print(movableRolls)
